46b51981b77db9865c66e127f176d13e7867630c
Saris OR <sasofia909@gmail.com>
2024-05-15 21:59:22 -0500

    prueba2

--- MarvelStudios/app/src/main/java/com/example/marvelstudios/LoginActivity.java
+++ MarvelStudios/app/src/main/java/com/example/marvelstudios/LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.marvelstudios;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,6 +15,8 @@ public class LoginActivity extends AppCompatActivity {
 
     EditText edtUser, edtPassword;
     Button btnLogin, btnRegister;
+    SharedPreferences sharedPreferences;
+    String savedUser, savedPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,20 +25,23 @@ public class LoginActivity extends AppCompatActivity {
         edtPassword = findViewById(R.id.edtPassword);
         btnLogin = findViewById(R.id.btnLogin);
         btnRegister = findViewById(R.id.btnRegister);
+        sharedPreferences = getSharedPreferences("user", Context.MODE_PRIVATE);
+        savedUser = sharedPreferences.getString("email", "");
+        savedPassword = sharedPreferences.getString("password", "");
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String user = edtUser.getText().toString();
                 String password = edtPassword.getText().toString();
-                if (user.equals("") || password.equals(""))
+                if (user.isEmpty() || password.isEmpty())
                     Toast.makeText(LoginActivity.this, "Por favor llenar los campos ", Toast.LENGTH_SHORT).show();
-                else if (user.equals("usuario") && password.equals("contraseña")) {
+                else if (user.equals(savedUser) && password.equals(savedPassword)) {
                     Intent i = new Intent(LoginActivity.this, MainActivity.class);
                     startActivity(i);
                     finish();
                 } else {
-                    Toast.makeText(LoginActivity.this, "Por favor llenar los campos ", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(LoginActivity.this, "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show();
                 }
             }
         });
--- MarvelStudios/app/src/main/java/com/example/marvelstudios/RegisterActivity.java
+++ MarvelStudios/app/src/main/java/com/example/marvelstudios/RegisterActivity.java
@@ -2,16 +2,17 @@ package com.example.marvelstudios;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
-
-import java.util.ArrayList;
+import java.util.Calendar;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -27,7 +28,14 @@ public class RegisterActivity extends AppCompatActivity {
         edtDate = findViewById(R.id.edtDate);
         edtPassword = findViewById(R.id.edtPassword);
         btnRegister = findViewById(R.id.btnRegister);
-
+        final SharedPreferences[] sharedPreferences = new SharedPreferences[1];
+        final SharedPreferences.Editor[] editor = new SharedPreferences.Editor[1];
+        edtDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog();
+            }
+        });
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -38,12 +46,35 @@ public class RegisterActivity extends AppCompatActivity {
                 if (name.equals("") || email.equals("") || date.equals("") ||password.equals(""))
                     Toast.makeText(RegisterActivity.this, "Por favor llenar los campos ", Toast.LENGTH_SHORT).show();
                 else {
-                    Toast.makeText(RegisterActivity.this, "Usuario registrado, por favor inicia sesión ", Toast.LENGTH_SHORT).show();
+                    sharedPreferences[0] = getSharedPreferences("user", Context.MODE_PRIVATE);
+                    editor[0] = sharedPreferences[0].edit();
+                    editor[0].putString("name", name);
+                    editor[0].putString("email", email);
+                    editor[0].putString("date", date);
+                    editor[0].putString("password", password);
+                    editor[0].apply();
+                    Toast.makeText(RegisterActivity.this, "Usuario registrado con exito", Toast.LENGTH_SHORT).show();
                     Intent i = new Intent(RegisterActivity.this, LoginActivity.class);
                     startActivity(i);
                     finish();
                 }
             }
         });
+    }
+    private void showDatePickerDialog() {
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year,
+                                          int monthOfYear, int dayOfMonth) {
+                        edtDate.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                    }
+                }, year, month, day);
+        datePickerDialog.show();
     }
 }
